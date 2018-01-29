c5814db3284f503ee0c807f401a9d5afe5f6373c
Igor Brodt <brodt@playform.com>
2018-01-29 10:38:51 +0300

    structure

---
+++ main/adminBuilder/frontBuilder.go
@@ -0,0 +1,7 @@
+package adminBuilder
+
+import "fmt"
+
+func Test() {
+	fmt.Println("Hello, 世界")
+}
---
+++ main/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"./adminBuilder"
+)
+
+
+func main() {
+	adminBuilder.Test();
+}
